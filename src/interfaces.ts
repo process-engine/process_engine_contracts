78b76bc5700e8c16408d3700cec29b14bc282bac
5ebastianMeier <sebastian.meier@5minds.de>
2017-01-25 13:22:13 +0100

    Add entity interface

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,9 +1,14 @@
 import {BpmnDiagram} from './bpmn_diagram';
-import {ExecutionContext} from '@process-engine-js/core_contracts';
+import {ExecutionContext, IEntity} from '@process-engine-js/core_contracts';
 
 export interface IProcessDefEntityTypeService {
   importBpmnFromXml(xml: string, context: ExecutionContext): Promise<void>;
   importBpmnFromFile(xml: string): Promise<void>;
   parseBpmnXml(xml: string): Promise<BpmnDiagram>;
   parseBpmnFile(path: string): Promise<BpmnDiagram>;
+}
+
+export interface IProcessDefEntity extends IEntity {
+  name: string;
+  xml: string;
 }
