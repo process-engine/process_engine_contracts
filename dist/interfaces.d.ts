ebf72e0d56fe5d9701a5820d56a1ac860ed1353f
5ebastianMeier <sebastian.meier@5minds.de>
2017-01-27 08:54:48 +0100

    Add interfaces for entity types

--- dist/interfaces.d.ts
+++ dist/interfaces.d.ts
@@ -6,7 +6,86 @@ export interface IProcessDefEntityTypeService {
     parseBpmnXml(xml: string): Promise<BpmnDiagram>;
     parseBpmnFile(path: string): Promise<BpmnDiagram>;
 }
+export interface IEndEventEntity extends IEventEntity {
+}
+export interface IEventEntity extends INodeInstanceEntity {
+}
+export interface IExclusiveGatewayEntity extends INodeInstanceEntity {
+    follow: any;
+}
+export interface IFlowDefEntity extends IEntity {
+    name: string;
+    key: string;
+    getProcessDef(): Promise<IProcessDefEntity>;
+    setProcessDef(value: IProcessDefEntity): void;
+    getSource(): Promise<INodeDefEntity>;
+    setSource(value: INodeDefEntity): void;
+    getTarget(): Promise<INodeDefEntity>;
+    setTarget(value: INodeDefEntity): void;
+    condition: string;
+}
+export interface ILaneEntity extends IEntity {
+    name: string;
+    key: string;
+    extensions: any;
+    getProcessDef(): Promise<IProcessDefEntity>;
+    setProcessDef(value: IProcessDefEntity): void;
+}
+export interface INodeDefEntity extends IEntity {
+    name: string;
+    key: string;
+    getProcessDef(): Promise<IProcessDefEntity>;
+    setProcessDef(value: IProcessDefEntity): void;
+    getLane(): Promise<ILaneEntity>;
+    setLane(value: ILaneEntity): void;
+    type: string;
+    extensions: any;
+    getAttachedToNode(): Promise<INodeDefEntity>;
+    setAttachedToNode(value: INodeDefEntity): void;
+    events: string;
+    getLaneRole(context: ExecutionContext): Promise<string>;
+}
+export interface INodeInstanceEntity extends IEntity {
+    name: string;
+    key: string;
+    getProcess(): Promise<IProcessEntity>;
+    setProcess(value: IProcessEntity): void;
+    getNodeDef(): Promise<INodeDefEntity>;
+    setNodeDef(value: INodeDefEntity): void;
+    type: string;
+    state: string;
+    participant: string;
+    getProcessToken(): Promise<IProcessTokenEntity>;
+    setProcessToken(value: IProcessTokenEntity): void;
+}
+export interface IParallelGatewayEntity extends INodeInstanceEntity {
+    parallelType: string;
+}
+export interface IProcessEntity extends IEntity {
+    name: string;
+    key: string;
+    getProcessDef(): Promise<IProcessDefEntity>;
+    setProcessDef(value: IProcessDefEntity): void;
+}
 export interface IProcessDefEntity extends IEntity {
     name: string;
+    key: string;
+    defId: string;
     xml: string;
+    start(context: ExecutionContext): Promise<void>;
+    updateDefinitions(context: ExecutionContext, newBpmnDiagram?: BpmnDiagram): Promise<void>;
+}
+export interface IProcessTokenEntity extends IEntity {
+    data: any;
+    getProcess(): Promise<IProcessEntity>;
+    setProcess(value: IProcessEntity): void;
+}
+export interface IScriptTaskEntity extends INodeInstanceEntity {
+    script: string;
+}
+export interface IServiceTaskEntity extends INodeInstanceEntity {
+}
+export interface IStartEventEntity extends IEventEntity {
+}
+export interface IUserTaskEntity extends INodeInstanceEntity {
 }
