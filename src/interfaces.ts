aba784327ef44bb40f18d3c49936a75cb1d524ab
Heiko Mathes <heiko.mathes@gmail.com>
2017-10-11 14:21:02 +0200

    :construction: split process.start

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -173,6 +173,7 @@ export interface IProcessEntity extends IEntity {
   status: string;
   getProcessDef(context: ExecutionContext): Promise<IProcessDefEntity>;
   processDef: IProcessDefEntity;
+  initializeProcess(): Promise<INodeDefEntity>;
   start(context: ExecutionContext, params: IParamStart, options?: IPublicGetOptions): Promise<void>;
   end(context: ExecutionContext, processToken: any): Promise<void>;
   error(context: ExecutionContext, error: any): Promise<void>;
