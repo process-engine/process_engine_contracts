0962ec0ef651e6171117646caa2f8b4923b69dba
5ebastianMeier <sebastian.meier@5minds.de>
2017-11-29 15:50:28 +0100

    add participant id to signature

--- src/iprocess_engine_service.ts
+++ src/iprocess_engine_service.ts
@@ -8,5 +8,5 @@ export interface IProcessEngineService {
   getUserTaskData(context: ExecutionContext, userTaskId: string): Promise<IUserTaskMessageData>;
   executeProcess(context: ExecutionContext, id: string, key: string, initialToken: any, version?: string): Promise<any>;
   createProcessInstance(context: ExecutionContext, processDefinitionId: string, key: string, version?: string): Promise<string>;
-  executeProcessInstance(context: ExecutionContext, processInstanceId: string, initialToken: any): Promise<any>;
+  executeProcessInstance(context: ExecutionContext, processInstanceId: string, participantId: string, initialToken: any): Promise<any>;
 }
