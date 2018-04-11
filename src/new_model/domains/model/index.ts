dff4d0931fee3d57b387c771eeb3e55c6eacc6e4
ElRaptorus <christian.werner@5minds.de>
2018-04-11 11:04:30 +0200

    Add missing export for new_model base component

--- src/new_model/domains/model/index.ts
+++ src/new_model/domains/model/index.ts
@@ -1,4 +1,5 @@
 import * as activities from './activities/index';
+import * as base from './base';
 import * as eventDefinitions from './event_definitions/index';
 import * as events from './events/index';
 import * as gateways from './gateways/index';
@@ -7,6 +8,7 @@ import * as types from './types/index';
 
 export namespace Model {
   export import Activities = activities;
+  export import Base = base;
   export import EventDefinitions = eventDefinitions;
   export import Events = events;
   export import Gateways = gateways;
@@ -19,6 +21,7 @@ export * from './imodel_reader';
 
 export const Model2 = {
   Activities: activities,
+  Base: base,
   EventDefinitions: eventDefinitions,
   Events: events,
   Gateways: gateways,
