1bab0963a76d1f86b3457805be10934d4bc29354
ElRaptorus <christian.werner@5minds.de>
2018-04-13 16:40:32 +0200

    Remove abstract declaration from flow element classes

--- src/new_model/domains/model/base/flow_node.ts
+++ src/new_model/domains/model/base/flow_node.ts
@@ -1,6 +1,6 @@
 import { BaseElement } from './base_element';
 
-export abstract class FlowNode extends BaseElement {
+export class FlowNode extends BaseElement {
   public name: string;
   public incoming: Array<any> = [];
   public outgoing: Array<any> = [];
--- src/new_model/domains/model/base/flow_sequence.ts
+++ src/new_model/domains/model/base/flow_sequence.ts
@@ -1,7 +1,7 @@
 import { BaseElement } from './base_element';
 import { ConditionExpression } from './condition_expression';
 
-export abstract class FlowSequence extends BaseElement {
+export class FlowSequence extends BaseElement {
   public name: string;
   public sourceRef: string;
   public targetRef: string;
