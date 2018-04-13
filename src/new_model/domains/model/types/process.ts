ca55097636066134c1e2d228f2edf341016831ce
ElRaptorus <christian.werner@5minds.de>
2018-04-13 15:35:14 +0200

    Add isExecutable flag to process object-model

--- src/new_model/domains/model/types/process.ts
+++ src/new_model/domains/model/types/process.ts
@@ -2,6 +2,7 @@ import {FlowElement, RootElement} from '../base/index';
 
 export class Process extends RootElement {
   public name: string;
+  public isExecutable: boolean;
   public laneSets: Array<FlowElement> = [];
   public flowElements: Array<FlowElement> = [];
 }
