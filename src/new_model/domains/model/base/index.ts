63df725add0a1b99980188eccce7318a19001736
ElRaptorus <christian.werner@5minds.de>
2018-04-17 09:56:34 +0200

    Add IConstructor interface to allow instance creation, using generic types

--- src/new_model/domains/model/base/index.ts
+++ src/new_model/domains/model/base/index.ts
@@ -3,6 +3,7 @@ export * from './camunda_execution_listener';
 export * from './camunda_extension_property';
 export * from './extension_elements';
 export * from './flow_node';
+export * from './iconstructor';
 export * from './interface';
 export * from './root_element';
 export * from './structure';
