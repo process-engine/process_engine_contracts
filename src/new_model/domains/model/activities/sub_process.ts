d07652079e706a10cb0dd102b422ab1712eb7318
5ebastianMeier <sebastian.meier@5minds.de>
2018-05-28 17:06:23 +0200

    refactor sub process properties for usability

--- src/new_model/domains/model/activities/sub_process.ts
+++ src/new_model/domains/model/activities/sub_process.ts
@@ -1,12 +1,13 @@
-import { FlowNode } from '../base/index';
-import { LaneSet } from '../types/index';
-import { Activity } from './activity';
 import { BpmnType } from '../../../../constants';
+import { FlowNode } from '../base/index';
+import { LaneSet, SequenceFlow } from '../types/index';
+import { Activity } from './activity';
 
 export class SubProcess extends Activity {
   public get bpmnType(): BpmnType {
     return BpmnType.subProcess;
   }
-  public laneSets: Array<LaneSet> = [];
-  public items: Array<FlowNode> = [];
+  public laneSet?: LaneSet;
+  public flowNodes: Array<FlowNode> = [];
+  public sequenceFlows: Array<SequenceFlow> = [];
 }
