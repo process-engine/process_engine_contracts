b691cd23857312cbb9a7ccd656fd9c8baa26f975
5ebastianMeier <sebastian.meier@5minds.de>
2018-05-29 14:42:04 +0200

    add execution context retrieval

--- src/new_model/runtime/engine/iexecution_context_fascade.ts
+++ src/new_model/runtime/engine/iexecution_context_fascade.ts
@@ -1,4 +1,6 @@
+import { ExecutionContext } from '@essential-projects/core_contracts';
 
 export interface IExecutionContextFascade {
   getIdentityToken(): string;
+  getExecutionContext(): ExecutionContext;
 }
