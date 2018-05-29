24bbd24d7f07c42468f45e0ac616233a4daef06b
5ebastianMeier <sebastian.meier@5minds.de>
2018-05-29 16:18:07 +0200

    remove unused methods from process token fascade

--- src/new_model/runtime/engine/iprocess_token_fascade.ts
+++ src/new_model/runtime/engine/iprocess_token_fascade.ts
@@ -3,8 +3,6 @@ import { IProcessTokenResult } from './iprocess_token_result';
 
 export interface IProcessTokenFascade {
   addResultForFlowNode(flowNodeId: string, result: any): Promise<void>;
-  getResultForFlowNode(flowNodeId: string): Promise<IProcessTokenResult>;
-  getAllResultsForFlowNode(flowNodeId: string): Promise<Array<IProcessTokenResult>>;
   getProcessTokenFascadeForParallelBranch(): Promise<IProcessTokenFascade>;
   mergeTokenHistory(processTokenToMerge: IProcessTokenFascade): Promise<void>;
   getAllResults(): Promise<Array<IProcessTokenResult>>;
