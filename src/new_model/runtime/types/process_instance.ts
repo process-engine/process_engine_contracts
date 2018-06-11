e9c31095e405a1654e73fc79cdf6c1c45eaadd18
5ebastianMeier <sebastian.meier@5minds.de>
2018-06-11 17:06:42 +0200

    add process instance properties

--- src/new_model/runtime/types/process_instance.ts
+++ src/new_model/runtime/types/process_instance.ts
@@ -1,5 +1,7 @@
 export class ProcessInstance {
   public id: ProcessInstanceId;
+  public correlationId: string;
+  public processDefinition: Model.Types.Process;
 }
 
 export type ProcessInstanceId = string;
