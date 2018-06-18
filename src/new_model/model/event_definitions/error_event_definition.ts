3efe91c52e002f0368fc262e11e038366b6ddef7
robpal94 <robin.palkovits@5minds.de>
2018-06-18 13:52:14 +0200

    :ok_hand: Move the Error reference to the ErrorRef property

--- src/new_model/model/event_definitions/error_event_definition.ts
+++ src/new_model/model/event_definitions/error_event_definition.ts
@@ -2,6 +2,5 @@ import {EventDefinition} from './event_definition';
 import {Error} from "../types/error";
 
 export class ErrorEventDefinition extends EventDefinition {
-  public errorReference: string;
-  public errorObject: Error;
+  public errorReference: Error;
 }
