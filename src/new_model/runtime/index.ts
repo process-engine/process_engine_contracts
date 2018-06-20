1a2b05828338427dd8944e301b1812eafa42be8e
5ebastianMeier <sebastian.meier@5minds.de>
2018-06-20 17:36:01 +0200

    remove old runtime exports

--- src/new_model/runtime/index.ts
+++ src/new_model/runtime/index.ts
@@ -1,17 +1,7 @@
-import * as activities from '../model/activities/index';
-// import * as eventDefinitions from './event_definitions/index';
-import * as events from '../model/events/index';
-// import * as gateways from './gateways/index';
-// import * as tasks from './tasks/index';
 import * as types from './types/index';
 
 // tslint:disable-next-line:no-namespace
 export namespace Runtime {
-  export import Activities = activities;
-  // export import EventDefinitions = eventDefinitions;
-  export import Events = events;
-  // export import Gateways = gateways;
-  // export import Tasks = tasks;
   export import Types = types;
 }
 
