08c64121a5e0b4f8443e0203e8bf17b96ecc9d30
5ebastianMeier <sebastian.meier@5minds.de>
2018-06-20 10:00:20 +0200

    update process token typings

--- src/new_model/runtime/types/process_token.ts
+++ src/new_model/runtime/types/process_token.ts
@@ -1,12 +1,14 @@
 import { ProcessInstance } from './process_instance';
+import { IIdentity } from '@essential-projects/core_contracts';
 
 export class ProcessToken {
   // header
   public processInstanceId: string;
-  public processModelId: string; // TODO: make use of this in queries
+  public processModelId: string;
   public correlationId: string;
-  public identity: any;
+  public identity: IIdentity;
   public createdAt: Date;
+  public caller: string; // empty if this started the correlation
   // payload
   public payload: any;
 }
