f7b0cf5575b8b029af09a71d8ba8244089e46eb8
5ebastianMeier <sebastian.meier@5minds.de>
2018-06-25 16:17:14 +0200

    apply review feedback

--- src/new_model/runtime/storage/iprocess_model_persistance.ts
+++ src/new_model/runtime/storage/iprocess_model_persistance.ts
@@ -1,6 +1,6 @@
 import { Definitions, Model, Runtime } from '../..';
 
-export interface IProcessModelPersistance {
+export interface IProcessModelPersistence {
   persistProcessDefinitions(definitions: Definitions): Promise<void>;
   getProcessModelById(processModelId: string): Promise<Model.Types.Process>;
   getProcessModels(): Promise<Array<Model.Types.Process>>;
