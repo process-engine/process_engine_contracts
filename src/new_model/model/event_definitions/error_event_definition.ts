945efbeaaf54b50f7177eec6f998a4b751154c83
ElRaptorus <christian.werner@5minds.de>
2018-06-26 10:24:37 +0200

    Fix styling

--- src/new_model/model/event_definitions/error_event_definition.ts
+++ src/new_model/model/event_definitions/error_event_definition.ts
@@ -1,5 +1,5 @@
+import {Error} from '../types/error';
 import {EventDefinition} from './event_definition';
-import {Error} from "../types/error";
 
 export class ErrorEventDefinition extends EventDefinition {
   public errorReference: Error;
