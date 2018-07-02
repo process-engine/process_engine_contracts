4fec276d3f1578754b33f76572961617fc677442
ElRaptorus <christian.werner@5minds.de>
2018-07-02 14:32:19 +0200

    Remove iam facade factory

--- src/new_model/runtime/engine/iiam_facade_factory.ts
+++
@@ -1,7 +0,0 @@
-import {IExecutionContextFacade, Model} from './../../index';
-
-import {IIamFacade} from './iiam_facade';
-
-export interface IIamFacadeFactory {
-  create(executionContextFacade: IExecutionContextFacade, processModel: Model.Types.Process): IIamFacade;
-}
