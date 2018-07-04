c2bd26bb32db5eee60bee1200aa7bc2148824ea0
ElRaptorus <christian.werner@5minds.de>
2018-07-04 16:59:33 +0200

    Implement review notes

--- src/new_model/runtime/engine/execution_context.ts
+++ src/new_model/runtime/engine/execution_context.ts
@@ -1,5 +1,8 @@
 import {IIdentity} from '@essential-projects/iam_contracts';
 
+// The execution context stores any user-related information that is relevant for the current running request.
+// Right now, this includes the users identity. In future versions this may also include things like
+// information about the users language and locale, or other request-related data.
 export class ExecutionContext {
 
   private _identity: IIdentity;
--- src/new_model/runtime/storage/index.ts
+++ src/new_model/runtime/storage/index.ts
@@ -1,4 +1,4 @@
 export * from './iflow_node_instance_persistence_repository';
-export * from './iprocess_model_persistence_repository';
 export * from './iflow_node_instance_persistence_service';
+export * from './iprocess_model_persistence_repository';
 export * from './iprocess_model_persistence_service';
