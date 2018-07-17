4010f0b4cf8e9b6d16bbe71189fa1d0e4ed05039
ElRaptorus <christian.werner@5minds.de>
2018-07-17 10:52:39 +0200

    Remove import functions from processDefEntityTypeService and ProcessEngineService

--- src/iprocess_def_entity_type_service.ts
+++ src/iprocess_def_entity_type_service.ts
@@ -1,12 +1,7 @@
 import {ExecutionContext, IEntityReference, IPublicGetOptions} from '@essential-projects/core_contracts';
 import {IParamStart, IProcessDefEntity} from './entities/process/index';
-import {IImportFromFileOptions, IImportFromXmlOptions, IParamImportFromFile, IParamImportFromXml} from './import/index';
-import {IBpmnDiagram} from './index';
 
 export interface IProcessDefEntityTypeService {
-  importBpmnFromXml(context: ExecutionContext, param: IParamImportFromXml, options?: IImportFromXmlOptions): Promise<void>;
-  importBpmnFromFile(context: ExecutionContext, param: IParamImportFromFile, options?: IImportFromFileOptions): Promise<void>;
-  parseBpmnXml(xml: string): Promise<IBpmnDiagram>;
   start(context: ExecutionContext, param: IParamStart, options?: IPublicGetOptions): Promise<IEntityReference>;
   getProcessDefinitionByKey(context: ExecutionContext,
                             processDefinitionKey: string,
--- src/iprocess_engine_service.ts
+++ src/iprocess_engine_service.ts
@@ -13,6 +13,5 @@ export interface IProcessEngineService {
   executeProcess(context: NewExecutionContext, id: string, key: string, initialToken: any, version?: string): Promise<any>;
   createProcessInstance(context: ExecutionContext, processDefinitionId: string, key: string, version?: string): Promise<string>;
   executeProcessInstance(context: ExecutionContext, processInstanceId: string, participantId: string, initialToken: any): Promise<any>;
-  createBpmnFromXml(context: ExecutionContext, xml: string, name: string): Promise<IProcessDefEntity>;
   setErrorDeserializer(deserializer: IErrorDeserializer): void;
 }
