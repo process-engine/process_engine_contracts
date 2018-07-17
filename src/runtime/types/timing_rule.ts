0ac32070a8c4203afaa5c0033e3d10f24c64ec3e
ElRaptorus <christian.werner@5minds.de>
2018-07-17 16:10:45 +0200

    Mark properties in timing_rule as optional

--- src/runtime/types/timing_rule.ts
+++ src/runtime/types/timing_rule.ts
@@ -1,9 +1,9 @@
 export class TimingRule {
-  public second: number;
-  public minute: number;
-  public hour: number;
-  public date: number;
-  public month: number;
-  public year: number;
-  public dayOfWeek: number;
+  public second?: number;
+  public minute?: number;
+  public hour?: number;
+  public date?: number;
+  public month?: number;
+  public year?: number;
+  public dayOfWeek?: number;
 }
