f28406ac09ecff8ba4cae0b1961af4d274c2527f
ElRaptorus <christian.werner@5minds.de>
2018-07-24 10:31:07 +0200

    Add getProcessDefinitionById to process definition repository interface

--- src/runtime/storage/iprocess_definition_repository.ts
+++ src/runtime/storage/iprocess_definition_repository.ts
@@ -3,4 +3,5 @@ import {ProcessDefinitionRaw} from './process_definition_raw';
 export interface IProcessDefinitionRepository {
   persistProcessDefinitions(name: string, xml: string, overwriteExisting?: boolean): Promise<void>;
   getProcessDefinitions(): Promise<Array<ProcessDefinitionRaw>>;
+  getProcessDefinitionByName(definitionId: string): Promise<ProcessDefinitionRaw>;
 }
