023486a9f4fb714a63c74d75852ac913972fa88d
ElRaptorus <christian.werner@5minds.de>
2018-07-27 14:32:51 +0200

    Remove unused remnants of the old implementation

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,3 @@ export * from './runtime/index';
 
 export * from './constants';
 export * from './ierror_deserializer';
-export * from './iprocess_entry';
-export * from './iprocess_repository';
-export * from './shared';
