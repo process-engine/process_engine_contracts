bff22439980efb4e533dae8513a9d3ba0810c98f
5ebastianMeier <sebastian.meier@5minds.de>
2018-08-20 11:56:55 +0200

    add reference to previous flow node to NextFlowNodeInfo

--- src/runtime/engine/next_flow_node_info.ts
+++ src/runtime/engine/next_flow_node_info.ts
@@ -2,14 +2,16 @@ import {IProcessTokenFacade} from '../index';
 
 import {Model, Runtime} from './../../index';
 
-export class NextFlowNodeInfo {
+export class NextFlowNodeInfo<TNextFlowNode extends Model.Base.FlowNode> {
 
-  public flowNode: Model.Base.FlowNode;
+  public flowNode: TNextFlowNode;
+  public previousFlowNode: Model.Base.FlowNode;
   public processTokenFacade: IProcessTokenFacade;
   public token: Runtime.Types.ProcessToken;
 
-  constructor(flowNode: Model.Base.FlowNode, token: Runtime.Types.ProcessToken, processTokenFacade: IProcessTokenFacade) {
+  constructor(flowNode: TNextFlowNode, previousFlowNode: Model.Base.FlowNode, token: Runtime.Types.ProcessToken, processTokenFacade: IProcessTokenFacade) {
     this.flowNode = flowNode;
+    this.previousFlowNode = previousFlowNode;
     this.token = token;
     this.processTokenFacade = processTokenFacade;
   }
