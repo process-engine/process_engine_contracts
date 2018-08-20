0e4911bd5ac8c389ca91b9906c678d77e8bc8727
5ebastianMeier <sebastian.meier@5minds.de>
2018-08-20 10:48:28 +0200

    add factory for process token facade

--- src/runtime/engine/index.ts
+++ src/runtime/engine/index.ts
@@ -6,6 +6,7 @@ export * from './iflow_node_handler_factory';
 export * from './iflow_node_handler';
 export * from './iprocess_model_facade_factory';
 export * from './iprocess_model_facade';
+export * from './iprocess_token_facade_factory';
 export * from './iprocess_token_facade';
 export * from './iprocess_token_result';
 export * from './next_flow_node_info';
