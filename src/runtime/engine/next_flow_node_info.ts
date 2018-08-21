620c0667784b6dc8056189a54e0c7100a5888095
5ebastianMeier <sebastian.meier@5minds.de>
2018-08-21 15:05:55 +0200

    remove previous flow node instance reference

--- src/runtime/engine/next_flow_node_info.ts
+++ src/runtime/engine/next_flow_node_info.ts
@@ -5,13 +5,11 @@ import {Model, Runtime} from './../../index';
 export class NextFlowNodeInfo<TNextFlowNode extends Model.Base.FlowNode> {
 
   public flowNode: TNextFlowNode;
-  public previousFlowNode: Model.Base.FlowNode;
   public processTokenFacade: IProcessTokenFacade;
   public token: Runtime.Types.ProcessToken;
 
-  constructor(flowNode: TNextFlowNode, previousFlowNode: Model.Base.FlowNode, token: Runtime.Types.ProcessToken, processTokenFacade: IProcessTokenFacade) {
+  constructor(flowNode: TNextFlowNode, token: Runtime.Types.ProcessToken, processTokenFacade: IProcessTokenFacade) {
     this.flowNode = flowNode;
-    this.previousFlowNode = previousFlowNode;
     this.token = token;
     this.processTokenFacade = processTokenFacade;
   }
