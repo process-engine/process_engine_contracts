4c2f687e7045ef4a9042697dbc120208f4bbb122
ElRaptorus <christian.werner@5minds.de>
2018-08-21 14:50:26 +0200

    Remove execution context facade from storage interfaces

--- src/runtime/storage/iflow_node_instance_repository.ts
+++ src/runtime/storage/iflow_node_instance_repository.ts
@@ -1,12 +1,12 @@
 import {FlowNodeInstance, FlowNodeInstanceState, ProcessToken} from './../types';
 
 export interface IFlowNodeInstanceRepository {
-  persistOnEnter(token: ProcessToken, flowNodeId: string, flowNodeInstanceId: string): Promise<FlowNodeInstance>;
-  persistOnExit(token: ProcessToken, flowNodeId: string, flowNodeInstanceId: string): Promise<FlowNodeInstance>;
-  persistOnError(token: ProcessToken, flowNodeId: string, flowNodeInstanceId: string, error: Error): Promise<FlowNodeInstance>;
-  persistOnTerminate(token: ProcessToken, flowNodeId: string, flowNodeInstanceId: string): Promise<FlowNodeInstance>;
-  suspend(token: ProcessToken, flowNodeInstanceId: string, correlationHash?: string): Promise<FlowNodeInstance>;
-  resume(flowNodeInstanceId: string): Promise<FlowNodeInstance>;
+  persistOnEnter(flowNodeId: string, flowNodeInstanceId: string, token: ProcessToken): Promise<FlowNodeInstance>;
+  persistOnExit(flowNodeId: string, flowNodeInstanceId: string, token: ProcessToken): Promise<FlowNodeInstance>;
+  persistOnError(flowNodeId: string, flowNodeInstanceId: string, token: ProcessToken, error: Error): Promise<FlowNodeInstance>;
+  persistOnTerminate(flowNodeId: string, flowNodeInstanceId: string, token: ProcessToken): Promise<FlowNodeInstance>;
+  suspend(flowNodeId: string, flowNodeInstanceId: string, token: ProcessToken): Promise<FlowNodeInstance>;
+  resume(flowNodeId: string, flowNodeInstanceId: string, token: ProcessToken): Promise<FlowNodeInstance>;
   queryByState(state: FlowNodeInstanceState): Promise<Array<FlowNodeInstance>>;
   queryByCorrelation(correlationId: string): Promise<Array<FlowNodeInstance>>;
   queryByProcessModel(processModelId: string): Promise<Array<FlowNodeInstance>>;
