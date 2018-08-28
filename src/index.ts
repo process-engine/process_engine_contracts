0fcb064638a0c82aa0849d7c2ecde8412385b55b
ElRaptorus <christian.werner@5minds.de>
2018-08-28 15:47:14 +0200

    :fire: Remove obsolete components

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,4 @@ export * from './runtime/index';
 
 export * from './constants';
 export * from './ierror_deserializer';
-export * from './iprocess_entry';
-export * from './iprocess_repository';
 export * from './shared';
