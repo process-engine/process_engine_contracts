58008871a7fa398192eb9c360297c35c32f702a5
ElRaptorus <christian.werner@5minds.de>
2018-10-10 10:23:25 +0200

    :fire: Remove obsolete WebServiceInvocation

--- src/model/activities/tasks/invocations/index.ts
+++ src/model/activities/tasks/invocations/index.ts
@@ -1,4 +1,3 @@
 export * from './external_task_invocation';
 export * from './invocation';
-export * from './web_service_invocation';
 export * from './method_invocation';
--- src/model/activities/tasks/invocations/web_service_invocation.ts
+++
@@ -1,9 +0,0 @@
-import {Invocation} from './invocation';
-
-/**
- * Used by ServiceTasks.
- * This contains the definition for a WebService invocation.
- */
-export class WebServiceInvocation extends Invocation {
-  public service: string;
-}
