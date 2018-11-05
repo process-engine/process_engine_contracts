39df7e0727281d78b94a8f8dc1b36342dddb3ec8
ElRaptorus <christian.werner@5minds.de>
2018-11-05 09:41:39 +0100

    :fire: Remove unnecessary references

--- src/model/type_references/error_reference.ts
+++
@@ -1,4 +0,0 @@
-// TODO: Not used anywhere - Do we still need this?
-export class ErrorReference {
-  public errorId: string;
-}
--- src/model/type_references/message_reference.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Contains the ID and expression of a message.
- * This is the message used by MessageEvents.
- */
-export class MessageReference {
-  public messageId: string;
-  public expression?: string;
-}
