c02339ac4ad284e68db580bbcae62194ce5fe4e2
robpal94 <robin.palkovits@5minds.de>
2018-11-09 15:55:32 +0100

    :ok_hand: add requested changes

--- src/model/activities/tasks/receive_task.ts
+++ src/model/activities/tasks/receive_task.ts
@@ -5,7 +5,15 @@ import {Task} from './task';
 /**
  * Describes a BPMN ReceiveTask.
  *
- * Task is used for receiving messages.
+ * A ReceiveTask is basically the opposite of a SendTask.
+ * It will block the Process Execution, until an expected Message is
+ * received.
+ *
+ * In contrast to an IntermediateCatchEvent, a ReceiveTask will
+ * throw back a confirmation Message that the ReceiveTask received the
+ * defined message.
+ * Therefore, a ReceiveTask will only accept messages, that where send by
+ * a SendTask.
  */
 export class ReceiveTask extends Task {
   public get bpmnType(): BpmnType {
@@ -13,9 +21,8 @@ export class ReceiveTask extends Task {
   }
 
  /**
-   * When using an IntermediateMessageCatchEvent, this will contain the message
-   * to wait for.
+   * This is the user defined Message which is expected by this
+   * ReceiveTask.
    */
-  public messageEventDefinition?: MessageEventDefinition;
-
+  public messageEventDefinition: MessageEventDefinition;
 }
--- src/model/activities/tasks/send_task.ts
+++ src/model/activities/tasks/send_task.ts
@@ -1,22 +1,24 @@
 import {BpmnType} from '../../../constants';
-import {Task} from './task';
 import {MessageEventDefinition} from '../../event_definitions';
+import {Task} from './task';
 
 /**
  * Describes a BPMN SendTask.
  *
- * Task is used for sending messages.
+ * A send task sends a user defined message to a ReceiveTask.
+ *
+ * In contrast to an Intermediate Message, a SendTask will block
+ * the Process Execution, until a confirmation message from a
+ * ReceiveTask was fetched.
  */
 export class SendTask extends Task {
   public get bpmnType(): BpmnType {
     return BpmnType.sendTask;
-  };
+  }
 
  /**
-   * When using an IntermediateMessageCatchEvent, this will contain the message
-   * to wait for.
+   * This property contains the user defined message which should be
+   * send to a receive task.
   ‚*/
-  public messageEventDefinition?: MessageEventDefinition;
-
-
+  public messageEventDefinition: MessageEventDefinition;
 }
