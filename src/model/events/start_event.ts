063dc6f93cabbb8a2b08df2b554fb23574c43c9b
bastian schnittger <bastian.schnittger@5minds.de>
2018-11-13 10:11:47 +0100

    Add EventType to StartEvent

--- src/model/events/start_event.ts
+++ src/model/events/start_event.ts
@@ -1,4 +1,4 @@
-import {BpmnType} from '../../constants';
+import {BpmnType, EventType} from '../../constants';
 import {Event} from './event';
 
 import {
@@ -13,6 +13,29 @@ import {
 export class StartEvent extends Event {
   public get bpmnType(): BpmnType {
     return BpmnType.startEvent;
+  }
+
+  public get eventType(): EventType {
+    const eventIsMessageEvent: boolean = this.messageEventDefinition !== undefined &&
+                                         this.messageEventDefinition !== null;
+    if (eventIsMessageEvent) {
+
+      return EventType.messageEvent;
+    }
+
+    const eventIsSignalEvent: boolean = this.signalEventDefinition !== undefined &&
+                                        this.signalEventDefinition !== null;
+    if (eventIsSignalEvent) {
+
+      return EventType.signalEvent;
+    }
+
+    const eventIsTimerEvent: boolean = this.timerEventDefinition !== undefined &&
+                                       this.timerEventDefinition !== null;
+    if (eventIsTimerEvent) {
+
+      return EventType.timerEvent;
+    }
   }
 
   public messageEventDefinition?: MessageEventDefinition;
