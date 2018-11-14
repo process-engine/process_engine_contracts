737a33065f0687751159a9d2ce709862b9cd6f8a
Alex😎 <alexander.kasten@5minds.de>
2018-11-14 08:43:06 +0100

    :sparkles: Add ManualTask EventAggregator Settings

--- src/runtime/messages/event_aggregator_settings.ts
+++ src/runtime/messages/event_aggregator_settings.ts
@@ -2,6 +2,8 @@
 const messagePaths = {
   userTaskReached: 'user_task_reached',
   userTaskFinished: 'user_task_finished',
+  manualTaskReached: 'manual_task_reached',
+  manualTaskFinished: 'manual_task_finished',
   processTerminated: 'process_terminated',
   processEnded: 'process_ended',
 };
@@ -14,15 +16,23 @@ const routeParams = {
   processInstanceId: ':process_instance_id',
   processModelId: ':process_model_id',
   userTaskId: ':user_task_id',
+  manualTaskId: ':manual_task_id',
   signalReference: ':signal_ref',
 };
 
-// tslint:disable:max-line-length
 const routePaths = {
   finishUserTask:
-    `/processengine/correlation/${routeParams.correlationId}/processinstance/${routeParams.processInstanceId}/usertask/${routeParams.flowNodeInstanceId}/finish`,
+    `/processengine/correlation/${routeParams.correlationId}/processinstance/
+    ${routeParams.processInstanceId}/usertask/${routeParams.flowNodeInstanceId}/finish`,
   userTaskFinished:
-    `/processengine/correlation/${routeParams.correlationId}/processinstance/${routeParams.processInstanceId}/usertask/${routeParams.flowNodeInstanceId}/finished`,
+    `/processengine/correlation/${routeParams.correlationId}/processinstance/
+    ${routeParams.processInstanceId}/usertask/${routeParams.flowNodeInstanceId}/finished`,
+  finishManualTask:
+  `/processengine/correlation/${routeParams.correlationId}/processinstance/
+  ${routeParams.processInstanceId}/manualtask/${routeParams.flowNodeInstanceId}/finish`,
+  manualTaskFinished:
+    `/processengine/correlation/${routeParams.correlationId}/processinstance/
+    ${routeParams.processInstanceId}/manualtask/${routeParams.flowNodeInstanceId}/finished`,
   endEventReached: `/processengine/correlation/${routeParams.correlationId}/processmodel/${routeParams.processModelId}/ended`,
   messageEventReached: `/processengine/process/message/${routeParams.messageReference}`,
   sendTaskReached: `/processengine/process/sendtask/${routeParams.messageReference}`,
