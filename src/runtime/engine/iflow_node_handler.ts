b4ef60073dc409ec654ca0b0b901077005c41a46
ElRaptorus <christian.werner@5minds.de>
2018-11-14 15:01:24 +0100

    :sparkles: Add previousFlowNodeInstanceId to IFlowNodeHandlers execute function

--- src/runtime/engine/iflow_node_handler.ts
+++ src/runtime/engine/iflow_node_handler.ts
@@ -22,16 +22,19 @@ export interface IFlowNodeHandler<TFlowNode extends Model.Base.FlowNode> {
    * returned.
    *
    * @async
-   * @param   flowNode           The FlowNode to execute.
-   * @param   token              The current ProcessToken.
-   * @param   processTokenFacade The Facade for the current ProcessToken.
-   * @param   processModelFacade The Facade for the ProcessModel.
-   * @param   identity           Contains the users identity.
-   * @returns                    Information about the next FlowNode to run.
+   * @param   flowNode                   The FlowNode to execute.
+   * @param   token                      The current ProcessToken.
+   * @param   processTokenFacade         The Facade for the current ProcessToken.
+   * @param   processModelFacade         The Facade for the ProcessModel.
+   * @param   identity                   Contains the users identity.
+   * @param   previousFlowNodeInstanceId The ID of the previously run FNI.
+   * @returns                            Info about the next FlowNode to run.
    */
   execute(flowNode: TFlowNode,
           token: Runtime.Types.ProcessToken,
           processTokenFacade: IProcessTokenFacade,
           processModelFacade: IProcessModelFacade,
-          identity: IIdentity): Promise<NextFlowNodeInfo>;
+          identity: IIdentity,
+          previousFlowNodeInstanceId?: string,
+         ): Promise<NextFlowNodeInfo>;
 }
