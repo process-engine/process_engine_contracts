abd797b5e452c82cdf626dd02d18f50ce14e36bc
Alex😎 <alexander.kasten@5minds.de>
2018-11-14 11:08:55 +0100

    :lipstick: Cut template strings correctly
    Otherwise template strings will contain the break and spaces.
    
    Co-Authored-By: Leonhard Tobisch <leott@users.noreply.github.com>

--- src/runtime/messages/event_aggregator_settings.ts
+++ src/runtime/messages/event_aggregator_settings.ts
@@ -22,18 +22,17 @@ const routeParams = {
 
 const routePaths = {
   finishUserTask:
-    `/processengine/correlation/${routeParams.correlationId}/processinstance/
     `/processengine/correlation/${routeParams.correlationId}/processinstance/` +
     `${routeParams.processInstanceId}/usertask/${routeParams.flowNodeInstanceId}/finish`,
   userTaskFinished:
-    `/processengine/correlation/${routeParams.correlationId}/processinstance/
-    ${routeParams.processInstanceId}/usertask/${routeParams.flowNodeInstanceId}/finished`,
+    `/processengine/correlation/${routeParams.correlationId}/processinstance/` +
+    `${routeParams.processInstanceId}/usertask/${routeParams.flowNodeInstanceId}/finished`,
   finishManualTask:
-  `/processengine/correlation/${routeParams.correlationId}/processinstance/
-  ${routeParams.processInstanceId}/manualtask/${routeParams.flowNodeInstanceId}/finish`,
+    `/processengine/correlation/${routeParams.correlationId}/processinstance/` +
+    `${routeParams.processInstanceId}/manualtask/${routeParams.flowNodeInstanceId}/finish`,
   manualTaskFinished:
-    `/processengine/correlation/${routeParams.correlationId}/processinstance/
-    ${routeParams.processInstanceId}/manualtask/${routeParams.flowNodeInstanceId}/finished`,
+    `/processengine/correlation/${routeParams.correlationId}/processinstance/` +
+    `${routeParams.processInstanceId}/manualtask/${routeParams.flowNodeInstanceId}/finished`,
   endEventReached: `/processengine/correlation/${routeParams.correlationId}/processmodel/${routeParams.processModelId}/ended`,
   messageEventReached: `/processengine/process/message/${routeParams.messageReference}`,
   sendTaskReached: `/processengine/process/sendtask/${routeParams.messageReference}`,
