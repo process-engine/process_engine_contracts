e3108136e043ecd851cafa94c74779cc281c9e75
Steffen <Steffen.knaup@5minds.de>
2018-11-15 09:26:54 +0100

    :sparkles: Add Identity to DeleteProcessModel

--- src/runtime/storage/idelete_process_model_service.ts
+++ src/runtime/storage/idelete_process_model_service.ts
@@ -1,3 +1,5 @@
+import {IIdentity} from '@essential-projects/iam_contracts';
+
 /**
  * The Service for deleting processModels.
  */
@@ -7,7 +9,8 @@ export interface IDeleteProcessModelService {
    * Removes all processModels, correlations, externalTasks, flowNodeInstances and ProcessTokens with a specific ProcessModelId.
    *
    * @async
-   * @param    processModelId The ID of the processModel that should be removed.
+   * @param   identity          The executing users identity.
+   * @param   processModelId The ID of the processModel that should be removed.
    */
-  deleteProcessModel(processModelId: string): Promise<void>;
+  deleteProcessModel(identity: IIdentity, processModelId: string): Promise<void>;
 }
