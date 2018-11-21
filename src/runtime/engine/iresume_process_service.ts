b3037f8da339e711d9f0fdc1eb9f3f31a5b4dee2
ElRaptorus <christian.werner@5minds.de>
2018-11-21 15:26:21 +0100

    :sparkles: Add resumeProcessInstanceById to IResumeProcessService

--- src/runtime/engine/iresume_process_service.ts
+++ src/runtime/engine/iresume_process_service.ts
@@ -1,3 +1,18 @@
 export interface IResumeProcessService {
-  findAndResumeInterruptedProcessInstances(): Promise<void>;
+  /**
+   * Finds and resumes all ProcessInstances that were interrupted before.
+   *
+   * @async
+   */
+  findAndResumeInterruptedProcessInstances(): Promise<any>;
+
+  /**
+   * Resumes a specific ProcessInstance by its ID.
+   * Used primarily by CallActivities and Subprocesses.
+   *
+   * @async
+   * @param   processInstanceId The ID of the ProcessInstance to resume.
+   * @returns                   The ProcessInstance's result.
+   */
+  resumeProcessInstanceById(processInstanceId: string): Promise<any>;
 }
