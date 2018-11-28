fd3d1057e41a7ec4fa1529d766b20fd876169ea5
ElRaptorus <christian.werner@5minds.de>
2018-11-28 17:10:41 +0100

    :sparkles: Expect an identity when resuming processes

--- src/runtime/engine/iresume_process_service.ts
+++ src/runtime/engine/iresume_process_service.ts
@@ -1,19 +1,25 @@
-export interface IResumeProcessService {
-  /**
-   * Finds and resumes all ProcessInstances that were interrupted before.
-   *
-   * @async
-   */
-  findAndResumeInterruptedProcessInstances(): Promise<any>;
+import {IIdentity} from '@essential-projects/iam_contracts';
 
-  /**
-   * Resumes a specific ProcessInstance by its ID.
-   * Used primarily by CallActivities and Subprocesses.
-   *
-   * @async
-   * @param   processModelId    The ID of the ProcessModel to resume.
-   * @param   processInstanceId The ID of the ProcessInstance to resume.
-   * @returns                   The ProcessInstance's result.
-   */
-  resumeProcessInstanceById(processModelId: string, processInstanceId: string): Promise<any>;
+export interface IResumeProcessService {
+
+    /**
+     * Finds and resumes all ProcessInstances that were interrupted before.
+     *
+     * @async
+     * @param identity The identity with which to resume the ProcessInstances.
+     */
+    findAndResumeInterruptedProcessInstances(identity: IIdentity): Promise<any>;
+
+    /**
+     * Resumes a specific ProcessInstance by its ID.
+     * Used primarily by CallActivities and Subprocesses.
+     *
+     * @async
+     * @param   identity          The identity with which to resume the
+     *                            ProcessInstance.
+     * @param   processModelId    The ID of the ProcessModel to resume.
+     * @param   processInstanceId The ID of the ProcessInstance to resume.
+     * @returns                   The ProcessInstance's result.
+     */
+    resumeProcessInstanceById(identity: IIdentity, processModelId: string, processInstanceId: string): Promise<any>;
 }
