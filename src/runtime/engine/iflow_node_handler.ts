902c549b245ce84f3cfbb41826d2303fc3717eea
ElRaptorus <christian.werner@5minds.de>
2019-01-29 13:22:11 +0100

    :recycle: Change return type for IFlowNodeHandler.execute and resume to void

--- src/runtime/engine/iflow_node_handler.ts
+++ src/runtime/engine/iflow_node_handler.ts
@@ -5,49 +5,46 @@ import {FlowNodeInstance, ProcessToken} from '../types';
 import {
   IProcessModelFacade,
   IProcessTokenFacade,
-  NextFlowNodeInfo,
 } from './index';
+
 /**
  * Handles the execution of a single FlowNodeInstance.
  */
 export interface IFlowNodeHandler<TFlowNode extends FlowNode> {
+
   /**
    * Gets the instance ID of the FlowNode that this handler is responsible for.
    *
    * @returns The instance ID of the corresponding FlowNode.
    */
   getInstanceId(): string;
+
   /**
    * Gets the FlowNode this handler is supposed to execute.
    *
    * @returns The FlowNode for this handler.
    */
   getFlowNode(): TFlowNode;
+
   /**
    * Resumes the given FlowNodeInstance from the point at which it was
    * interrupted.
-   * After execution is done, information about the next FlowNode in line is
-   * returned.
-   *
-   * CAUTION:
-   * This is meant to be run by the "ResumeProcessService" ONLY!
    *
    * @async
    * @param   flowNodeInstance   The FlowNodeInstance to resume.
    * @param   processTokenFacade The Facade for the current ProcessToken.
    * @param   processModelFacade The Facade for the ProcessModel.
    * @param   identity           The Identity that started the FlowNodeInstance.
-   * @returns                    Info about the next FlowNode to run.
    */
-  resume(flowNodeInstance: FlowNodeInstance,
-         processTokenFacade: IProcessTokenFacade,
-         processModelFacade: IProcessModelFacade,
-         identity: IIdentity,
-        ): Promise<NextFlowNodeInfo>;
+  resume(
+    flowNodeInstance: FlowNodeInstance,
+    processTokenFacade: IProcessTokenFacade,
+    processModelFacade: IProcessModelFacade,
+    identity: IIdentity,
+  ): Promise<void>;
+
   /**
-   * Executes the given FlowNode.
-   * After execution is done, information about the next FlowNode in line is
-   * returned.
+   * Executes the FlowNode attached to this handler.
    *
    * @async
    * @param   token                      The current ProcessToken.
@@ -55,12 +52,12 @@ export interface IFlowNodeHandler<TFlowNode extends FlowNode> {
    * @param   processModelFacade         The Facade for the ProcessModel.
    * @param   identity                   Contains the users identity.
    * @param   previousFlowNodeInstanceId The ID of the previously run FNI.
-   * @returns                            Info about the next FlowNode to run.
    */
-  execute(token: ProcessToken,
-          processTokenFacade: IProcessTokenFacade,
-          processModelFacade: IProcessModelFacade,
-          identity: IIdentity,
-          previousFlowNodeInstanceId?: string,
-         ): Promise<NextFlowNodeInfo>;
+  execute(
+    token: ProcessToken,
+    processTokenFacade: IProcessTokenFacade,
+    processModelFacade: IProcessModelFacade,
+    identity: IIdentity,
+    previousFlowNodeInstanceId?: string,
+    ): Promise<void>;
 }
