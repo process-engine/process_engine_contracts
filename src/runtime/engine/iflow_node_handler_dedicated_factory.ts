8818f1460d5e9d246fb6d0534852be421d2ca6a4
ElRaptorus <christian.werner@5minds.de>
2019-02-04 14:36:02 +0100

    :recycle: Expect ProcessModelFacade and ProcessToken in dedicated factories

--- src/runtime/engine/iflow_node_handler_dedicated_factory.ts
+++ src/runtime/engine/iflow_node_handler_dedicated_factory.ts
@@ -1,5 +1,7 @@
 import {Model} from './../../model';
+import {ProcessToken} from './../../runtime/types';
 import {IFlowNodeHandler} from './iflow_node_handler';
+import {IProcessModelFacade} from './iprocess_model_facade';
 
 /**
  * Creates instances of a FlowNodeHandler for a specific FlowNode type.
@@ -8,12 +10,21 @@ import {IFlowNodeHandler} from './iflow_node_handler';
  * which use multiple handlers.
  */
 export interface IFlowNodeHandlerDedicatedFactory<TFlowNode extends Model.Base.FlowNode> {
+
   /**
    * Returns a new Instance of the FlowNodeHandler for the given FlowNode.
    *
    * @async
    * @param   flowNode           The FlowNode for which to create a handler.
+   * @param   processModelFacade Optional: A ProcessModelFacade to use.
+   * @param   processToken       Optional: The current ProcessToken.
+   *                             This can be used to store unique instances
+   *                             of a handler.
    * @returns                    The created FlowNodeHandler.
    */
-  create(flowNode: TFlowNode): Promise<IFlowNodeHandler<TFlowNode>>;
+  create(
+    flowNode: TFlowNode,
+    processModelFacade?: IProcessModelFacade,
+    processToken?: ProcessToken,
+  ): Promise<IFlowNodeHandler<TFlowNode>>;
 }
--- src/runtime/engine/iflow_node_handler_factory.ts
+++ src/runtime/engine/iflow_node_handler_factory.ts
@@ -1,4 +1,5 @@
 import {Model} from './../../model';
+import {ProcessToken} from './../../runtime/types';
 import {IFlowNodeHandler} from './iflow_node_handler';
 import {IProcessModelFacade} from './iprocess_model_facade';
 
@@ -12,7 +13,12 @@ export interface IFlowNodeHandlerFactory {
    * @async
    * @param   flowNode           The FlowNode for which to create a handler.
    * @param   processModelFacade Optional: A ProcessModelFacade to use.
+   * @param   processToken       Optional: The current ProcessToken.
    * @returns                    The created FlowNodeHandler.
    */
-  create<TFlowNode extends Model.Base.FlowNode>(flowNode: TFlowNode, processModelFacade?: IProcessModelFacade): Promise<IFlowNodeHandler<TFlowNode>>;
+  create<TFlowNode extends Model.Base.FlowNode>(
+    flowNode: TFlowNode,
+    processModelFacade?: IProcessModelFacade,
+    processToken?: ProcessToken,
+  ): Promise<IFlowNodeHandler<TFlowNode>>;
 }
