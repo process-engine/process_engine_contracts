6295e525ec42e5ae6ea7aaab8fea66f8aa81d28d
ElRaptorus <christian.werner@5minds.de>
2019-02-05 11:47:43 +0100

    :fire: Remove ProcessModelFacade from FlowNodeHandlerFactory

--- src/runtime/engine/iflow_node_handler_factory.ts
+++ src/runtime/engine/iflow_node_handler_factory.ts
@@ -1,7 +1,6 @@
 import {Model} from './../../model';
 import {ProcessToken} from './../../runtime/types';
 import {IFlowNodeHandler} from './iflow_node_handler';
-import {IProcessModelFacade} from './iprocess_model_facade';
 
 /**
  * Creates instances of the FlowNodeHandler.
@@ -11,14 +10,12 @@ export interface IFlowNodeHandlerFactory {
    * Returns a new Instance of the FlowNodeHandler for the given FlowNode and ProcessModelFacade.
    *
    * @async
-   * @param   flowNode           The FlowNode for which to create a handler.
-   * @param   processModelFacade Optional: A ProcessModelFacade to use.
-   * @param   processToken       Optional: The current ProcessToken.
-   * @returns                    The created FlowNodeHandler.
+   * @param   flowNode     The FlowNode for which to create a handler.
+   * @param   processToken Optional: The current ProcessToken.
+   * @returns              The created FlowNodeHandler.
    */
   create<TFlowNode extends Model.Base.FlowNode>(
     flowNode: TFlowNode,
-    processModelFacade?: IProcessModelFacade,
     processToken?: ProcessToken,
   ): Promise<IFlowNodeHandler<TFlowNode>>;
 }
