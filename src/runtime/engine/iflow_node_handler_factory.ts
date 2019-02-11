5a5d49645b1437b517c3781d7fa724463dd93348
ElRaptorus <christian.werner@5minds.de>
2019-02-11 17:03:38 +0100

    :recycle: Include BoundaryEventHandlerFactory in main factory

--- src/runtime/engine/iflow_node_handler_factory.ts
+++ src/runtime/engine/iflow_node_handler_factory.ts
@@ -1,13 +1,15 @@
 import {Model} from './../../model';
 import {ProcessToken} from './../../runtime/types';
+import {IBoundaryEventHandler} from './iboundary_event_handler';
 import {IFlowNodeHandler} from './iflow_node_handler';
 
 /**
  * Creates instances of the FlowNodeHandler.
  */
 export interface IFlowNodeHandlerFactory {
+
   /**
-   * Returns a new Instance of the FlowNodeHandler for the given FlowNode and ProcessModelFacade.
+   * Returns a new Instance of the FlowNodeHandler for the given FlowNode.
    *
    * @async
    * @param   flowNode     The FlowNode for which to create a handler.
@@ -15,4 +17,15 @@ export interface IFlowNodeHandlerFactory {
    * @returns              The created FlowNodeHandler.
    */
   create<TFlowNode extends Model.Base.FlowNode>(flowNode: TFlowNode, processToken?: ProcessToken): Promise<IFlowNodeHandler<TFlowNode>>;
+
+  /**
+   * Returns a new Instance of a handler for the given BoundaryEvent.
+   * BoundaryEvents work differently from the the other FlowNodes, therefore
+   * they are handled separately.
+   *
+   * @async
+   * @param   flowNode The BoundaryEvent for which to create a handler.
+   * @returns          The created BoundaryEventHandler.
+   */
+  createForBoundaryEvent(flowNode: Model.Events.BoundaryEvent): Promise<IBoundaryEventHandler>;
 }
