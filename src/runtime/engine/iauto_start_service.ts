200c022d7e59220dc3d7dab2061220cb41ca7bd6
Christian W <christian.werner@5minds.de>
2019-02-13 13:17:10 +0100

    :book: Improve some code comments

--- src/runtime/engine/iauto_start_service.ts
+++ src/runtime/engine/iauto_start_service.ts
@@ -1,27 +1,24 @@
 /**
- * This service is responsible for starting ProcessModels
- * that make use of StartEvents with signals and messages attached to it.
+ * This service is responsible for starting ProcessModels that make use of
+ * event based StartEvents.
  *
  * It does this, by subscribing to a global notification for messages and
  * signals.
  * Whenever a signal or message is received, the service will lookup all
- * ProcessModels that use matching StartEvents and triggers their execution.
+ * ProcessModels that use matching StartEvents and create new instances 
+ * for them.
  */
 export interface IAutoStartService {
 
   /**
    * Starts the Service.
-   * Creates all relevant subscriptions on the EventAggregator
-   * and initializes relevant EventHandlers.
    *
    * @async
    */
   start(): Promise<void>;
 
   /**
-   *
    * Stops the service.
-   * Remvoes all Subscriptions on the EventAggregator.
    *
    * @async
    */
