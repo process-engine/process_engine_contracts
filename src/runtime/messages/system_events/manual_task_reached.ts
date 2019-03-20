9b0374a3b775426fb6c53b5bc2846f85fe20b8b2
Steffen <Steffen.knaup@5minds.de>
2019-03-20 10:03:51 +0100

    :pencil2: Fix Typos

--- src/runtime/messages/system_events/manual_task_reached.ts
+++ src/runtime/messages/system_events/manual_task_reached.ts
@@ -1,6 +1,6 @@
 import {BaseSystemEventMessage} from './base_system_event_message';
 
 /**
- * Represents the message which is send, when a ManualTask has been finished.
+ * Represents the message which is send, when a ManualTask has been reached.
  */
 export class ManualTaskReachedMessage extends BaseSystemEventMessage {}
