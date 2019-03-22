1745d648ed02f62132c74ecb0f3aa178c2faf4db
ElRaptorus <christian.werner@5minds.de>
2019-03-22 13:18:04 +0100

    :sparkles: Add interrupted-state to duplicated FNI model

--- src/runtime/types/flow_node_instance_state.ts
+++ src/runtime/types/flow_node_instance_state.ts
@@ -5,6 +5,7 @@ export enum FlowNodeInstanceState {
   running = 'running',
   suspended = 'suspended',
   finished = 'finished',
+  interrupted = 'interrupted',
   terminated = 'terminated',
   error = 'error',
 }
