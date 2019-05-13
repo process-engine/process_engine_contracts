06a6d65c19d77895e6976099409d0addd5701d44
Steffen <Steffen.knaup@5minds.de>
2019-05-13 10:27:30 +0200

    :rotating_light: Fix Lint Errors

--- src/runtime/messages/system_events/call_activity_reached.ts
+++ src/runtime/messages/system_events/call_activity_reached.ts
@@ -1,19 +1,6 @@
-import {IIdentity} from '@essential-projects/iam_contracts';
-
 import {BaseSystemEventMessage} from './base_system_event_message';
 
 /**
  * The message sent when a call activity has been reached.
  */
-export class CallActivityReachedMessage extends BaseSystemEventMessage {
-
-  constructor(correlationId: string,
-              processModelId: string,
-              processInstanceId: string,
-              flowNodeId: string,
-              flowNodeInstanceId: string,
-              processInstanceOwner: IIdentity,
-              currentToken: any) {
-    super(correlationId, processModelId, processInstanceId, flowNodeId, flowNodeInstanceId, processInstanceOwner, currentToken);
-  }
-}
+export class CallActivityReachedMessage extends BaseSystemEventMessage {}
