c9117e99ca3de281988ec207efb75e7dab39c784
Steffen <Steffen.knaup@5minds.de>
2019-05-27 09:24:39 +0200

    :pencil2: Fix Typo

--- src/runtime/messages/system_events/intermediate_event_triggered.ts
+++ src/runtime/messages/system_events/intermediate_event_triggered.ts
@@ -1,6 +1,6 @@
 import {BaseSystemEventMessage} from './base_system_event_message';
 
 /**
- * The message sent when a intermediate event has been triggered.
+ * The message sent when an IntermediateEvent has been triggered.
  */
 export class IntermediateEventTriggeredMessage extends BaseSystemEventMessage {}
