fbb41f077a658e1339878dcb18b8c03951180dc0
Steffen <Steffen.knaup@5minds.de>
2019-05-27 09:24:21 +0200

    :lipstick: Fix Name of Event

--- src/runtime/messages/system_events/intermediate_event_finished.ts
+++ src/runtime/messages/system_events/intermediate_event_finished.ts
@@ -1,6 +1,6 @@
 import {BaseSystemEventMessage} from './base_system_event_message';
 
 /**
- * The message sent when a intermediate event has been finished.
+ * The message sent when an IntermediateCatchEvent has been finished.
  */
-export class IntermediateEventFinishedMessage extends BaseSystemEventMessage {}
+export class IntermediateCatchEventFinishedMessage extends BaseSystemEventMessage {}
