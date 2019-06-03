d168ac8799267184ad3935914d4640fab4c515c9
Steffen <Steffen.knaup@5minds.de>
2019-06-03 09:20:02 +0200

    :recycle: Refactor IntermediateEvent Events

--- src/runtime/messages/system_events/intermediate_event_triggered.ts
+++
@@ -1,6 +0,0 @@
-import {BaseSystemEventMessage} from './base_system_event_message';
-
-/**
- * The message sent when an IntermediateEvent has been triggered.
- */
-export class IntermediateEventTriggeredMessage extends BaseSystemEventMessage {}
