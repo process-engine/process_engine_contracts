8e350e7fa11039ef09da1472a6a3a86a3965799d
ElRaptorus <christian.werner@5minds.de>
2019-06-25 10:46:42 +0200

    :recycle: Derive UserTask messages directly from base message

--- src/runtime/messages/system_events/user_task_finished.ts
+++ src/runtime/messages/system_events/user_task_finished.ts
@@ -1,12 +1,12 @@
 import {IIdentity} from '@essential-projects/iam_contracts';
 
 import {UserTaskResult} from '../../types/user_task_result';
-import {ActivityFinishedMessage} from './activity_finished';
+import {BaseSystemEventMessage} from './base_system_event_message';
 
 /**
  * The message sent when a user task has been finished.
  */
-export class UserTaskFinishedMessage extends ActivityFinishedMessage {
+export class UserTaskFinishedMessage extends BaseSystemEventMessage {
 
   /**
    * The result the user task was finished with.
--- src/runtime/messages/system_events/user_task_reached.ts
+++ src/runtime/messages/system_events/user_task_reached.ts
@@ -1,6 +1,6 @@
-import {ActivityReachedMessage} from './activity_reached';
+import {BaseSystemEventMessage} from './base_system_event_message';
 
 /**
  * The message sent when a UserTask has been reached.
  */
-export class UserTaskReachedMessage extends ActivityReachedMessage {}
+export class UserTaskReachedMessage extends BaseSystemEventMessage {}
